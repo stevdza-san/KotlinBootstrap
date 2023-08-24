c4056b04aeae6bd087781aa94bd12c3e14eb778b
Stevdza-San <stefan.jovanavich@gmail.com>
2023-08-24 10:35:50 +0200

    Switch assets added.

--- site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
+++ site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
@@ -1,6 +1,7 @@
 package com.stevdza.san.kotlinbs.pages
 
 import androidx.compose.runtime.Composable
+import com.stevdza.san.kotlinbs.forms.BSSwitch
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -18,6 +19,28 @@ fun HomePage() {
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.Top
     ) {
-
+        BSSwitch(
+            modifier = Modifier.margin(right = 50.px),
+            label = "Android",
+            onClick = {}
+        )
+        BSSwitch(
+            modifier = Modifier.margin(right = 50.px),
+            label = "iOS",
+            defaultChecked = true,
+            onClick = {}
+        )
+        BSSwitch(
+            modifier = Modifier.margin(right = 50.px),
+            label = "Web",
+            disabled = true,
+            defaultChecked = true,
+            onClick = {}
+        )
+        BSSwitch(
+            label = "Desktop",
+            disabled = true,
+            onClick = {}
+        )
     }
 }
