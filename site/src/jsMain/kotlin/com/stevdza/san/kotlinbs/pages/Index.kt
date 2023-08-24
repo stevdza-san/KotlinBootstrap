f4af4a985c71d330bd14087243c040bd6d9c8c8a
Stevdza-San <stefan.jovanavich@gmail.com>
2023-08-24 09:01:42 +0200

    Checkbox asset added.

--- site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
+++ site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
@@ -1,85 +1,54 @@
 package com.stevdza.san.kotlinbs.pages
 
 import androidx.compose.runtime.*
-import com.stevdza.san.kotlinbs.forms.BSTextArea
-import com.stevdza.san.kotlinbs.models.InputValidation
+import com.stevdza.san.kotlinbs.forms.BSCheckbox
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
-import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.margin
-import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.core.Page
 import org.jetbrains.compose.web.css.px
 
 @Page
 @Composable
 fun HomePage() {
-    var value by remember { mutableStateOf("") }
-    Row(
-        modifier = Modifier.fillMaxSize().margin(top = 200.px),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.Top
+    Column(
+        modifier = Modifier.fillMaxSize().margin(left = 250.px),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(modifier = Modifier.margin(right = 24.px)) {
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                onValueChange = { value = it }
-            )
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                label = "Email Address",
-                placeholder = "Type here...",
-                floating = true,
-                onValueChange = { value = it }
-            )
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                label = "Email Address",
-                onValueChange = { value = it }
-            )
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                label = "Email Address",
-                placeholder = "Type here...",
-                onValueChange = { value = it }
-            )
-        }
-        Column {
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                label = "Email Address",
-                placeholder = "Type here...",
-                disabled = true,
-                onValueChange = { value = it }
-            )
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                label = "Email Address",
-                placeholder = "Type here...",
-                validation = InputValidation(
-                    isValid = true
-                ),
-                onValueChange = { value = it }
-            )
-            BSTextArea(
-                modifier = Modifier.width(250.px).margin(bottom = 12.px),
-                value = value,
-                label = "Email Address",
-                placeholder = "Type here...",
-                validation = InputValidation(
-                    isInvalid = true
-                ),
-                onValueChange = { value = it }
-            )
-        }
+        BSCheckbox(
+            modifier = Modifier.margin(bottom = 14.px),
+            label = "Kotlin",
+            onClick = {
+
+            }
+        )
+        BSCheckbox(
+            modifier = Modifier.margin(bottom = 14.px),
+            label = "Python",
+            disabled = true,
+            onClick = {
+
+            }
+        )
+        BSCheckbox(
+            modifier = Modifier.margin(bottom = 14.px),
+            label = "C++",
+            reverse = true,
+            onClick = {
+
+            }
+        )
+        BSCheckbox(
+            modifier = Modifier.margin(bottom = 14.px),
+            label = "PHP",
+            toggleButton = true,
+            onClick = {
+
+            }
+        )
     }
 }
