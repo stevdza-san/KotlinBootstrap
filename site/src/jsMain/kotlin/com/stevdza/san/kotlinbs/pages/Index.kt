80f33e6b071f128d7620279ade715c9786111946
Stevdza-San <stefan.jovanavich@gmail.com>
2023-08-24 11:04:03 +0200

    Alerts assets added.

--- site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
+++ site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
@@ -1,9 +1,11 @@
 package com.stevdza.san.kotlinbs.pages
 
 import androidx.compose.runtime.Composable
-import com.stevdza.san.kotlinbs.forms.BSSwitch
+import com.stevdza.san.kotlinbs.components.BSAlert
+import com.stevdza.san.kotlinbs.models.AlertIcon
+import com.stevdza.san.kotlinbs.models.AlertStyle
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
-import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
@@ -14,33 +16,52 @@ import org.jetbrains.compose.web.css.px
 @Page
 @Composable
 fun HomePage() {
-    Row(
-        modifier = Modifier.fillMaxSize().margin(top = 250.px),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.Top
+    Column (
+        modifier = Modifier.fillMaxSize().margin(left = 250.px),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
     ) {
-        BSSwitch(
-            modifier = Modifier.margin(right = 50.px),
-            label = "Android",
-            onClick = {}
+        BSAlert(
+            modifier = Modifier.margin(bottom = 24.px),
+            message = "Visit my YouTube Channel: Stevdza-San",
+            icon = AlertIcon.Info,
+            bold = "Stevdza-San"
         )
-        BSSwitch(
-            modifier = Modifier.margin(right = 50.px),
-            label = "iOS",
-            defaultChecked = true,
-            onClick = {}
+        BSAlert(
+            modifier = Modifier.margin(bottom = 24.px),
+            message = "You're not allowed to access this page.",
+            icon = AlertIcon.Warning,
+            style = AlertStyle.Warning
         )
-        BSSwitch(
-            modifier = Modifier.margin(right = 50.px),
-            label = "Web",
-            disabled = true,
-            defaultChecked = true,
-            onClick = {}
+        BSAlert(
+            modifier = Modifier.margin(bottom = 24.px),
+            message = "Unexpected error occurred.",
+            icon = AlertIcon.Warning,
+            style = AlertStyle.Danger
         )
-        BSSwitch(
-            label = "Desktop",
-            disabled = true,
-            onClick = {}
+        BSAlert(
+            message = "You have successfully purchased a book!",
+            alertLink = Pair(first = "book", second = "https://google.com"),
+            icon = AlertIcon.Checkmark,
+            style = AlertStyle.Success
+        )
+        BSAlert(
+            modifier = Modifier.margin(bottom = 24.px),
+            message = "Secondary Style alert.",
+            dismissible = true,
+            style = AlertStyle.Secondary
+        )
+        BSAlert(
+            modifier = Modifier.margin(bottom = 24.px),
+            message = "Light Style alert.",
+            dismissible = true,
+            style = AlertStyle.Light
+        )
+        BSAlert(
+            modifier = Modifier.margin(bottom = 24.px),
+            message = "Dark Style alert.",
+            dismissible = true,
+            style = AlertStyle.Dark
         )
     }
 }
