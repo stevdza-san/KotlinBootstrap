3c3394cd12263888775441b067a663cb7dab236d
Stevdza-San <stefan.jovanavich@gmail.com>
2023-08-25 08:06:27 +0200

    Tooltip asset added.

--- site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
+++ site/src/jsMain/kotlin/com/stevdza/san/kotlinbs/pages/Index.kt
@@ -1,52 +1,36 @@
 package com.stevdza.san.kotlinbs.pages
 
 import androidx.compose.runtime.Composable
-import com.stevdza.san.kotlinbs.components.BSSpinner
-import com.stevdza.san.kotlinbs.models.SpinnerVariant
+import androidx.compose.runtime.LaunchedEffect
+import com.stevdza.san.kotlinbs.components.BSTooltip
+import com.stevdza.san.kotlinbs.components.initializeTooltips
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
-import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.core.Page
-import org.jetbrains.compose.web.css.px
+import com.varabyte.kobweb.silk.components.text.SpanText
+import org.jetbrains.compose.web.dom.A
 
 @Page
 @Composable
 fun HomePage() {
+    LaunchedEffect(Unit) {
+        initializeTooltips()
+    }
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column(modifier = Modifier.margin(right = 40.px)) {
-            BSSpinner(
-                modifier = Modifier.margin(bottom = 20.px),
-                variant = SpinnerVariant.Small
-            )
-            BSSpinner(
-                modifier = Modifier.margin(bottom = 20.px),
-                variant = SpinnerVariant.SmallGrow
-            )
-        }
-        Column(modifier = Modifier.margin(right = 40.px)) {
-            BSSpinner(modifier = Modifier.margin(bottom = 20.px))
-            BSSpinner(
-                modifier = Modifier.margin(bottom = 20.px),
-                variant = SpinnerVariant.DefaultGrow
-            )
-        }
-        Column {
-            BSSpinner(
-                modifier = Modifier.margin(bottom = 20.px),
-                variant = SpinnerVariant.Large
-            )
-            BSSpinner(
-                modifier = Modifier.margin(bottom = 20.px),
-                variant = SpinnerVariant.LargeGrow
-            )
-        }
+        BSTooltip(
+            text = "https://stevdza-san.com",
+            content = {
+                A(href = "https://stevdza-san.com") {
+                    SpanText(text = "Online Courses")
+                }
+            }
+        )
     }
 }
